e9a6de62ab24451849af6a114726f4fe7973a46a
Fernando Marins <f.augustomarins@gmail.com>
2025-04-12 10:34:40 -0300

    Adding daily reading.

--- Ciganus/Ciganus/Card/CombinedCardsView.swift
+++ Ciganus/Ciganus/Card/CombinedCardsView.swift
@@ -128,8 +128,8 @@ struct CombinedCardsView: View {
             return
         }
 
-        let nomeCarta1 = nomesDasCartas["\(num1)"] ?? ""
-        let nomeCarta2 = nomesDasCartas["\(num2)"] ?? ""
+        let nomeCarta1 = Source.shared.nomesDasCartas["\(num1)"] ?? ""
+        let nomeCarta2 = Source.shared.nomesDasCartas["\(num2)"] ?? ""
 
         nameFirstCard = nomeCarta1
         nameSecondCard = nomeCarta2
@@ -144,14 +144,6 @@ struct CombinedCardsView: View {
 
         focusedField = nil
     }
-
-    private let nomesDasCartas: [String: String] = [
-        "1": "Cavaleiro", "2": "Trevo", "3": "Navio", "4": "Casa", "5": "Árvore", "6": "Nuvens", "7": "Cobra", "8": "Caixão",
-        "9": "Buquê", "10": "Foice", "11": "Chicote", "12": "Pássaros", "13": "Criança", "14": "Raposa", "15": "Urso", "16": "Estrelas",
-        "17": "Cegonha", "18": "Cachorro", "19": "Torre", "20": "Jardim", "21": "Montanha", "22": "Caminhos", "23": "Rato", "24": "Coração",
-        "25": "Anel", "26": "Livros", "27": "Carta", "28": "Homem", "29": "Mulher", "30": "Lírios", "31": "Sol", "32": "Lua",
-        "33": "Chave", "34": "Peixes", "35": "Âncora", "36": "Cruz"
-    ]
 }
 
 struct CombinedCardModel: Decodable {
@@ -178,7 +170,7 @@ class CombinedCardsModel {
         do {
             let data = try Data(contentsOf: fileURL)
             let decoder = JSONDecoder()
-            let decodedObject = try decoder.decode([CombinedCardModel].self, from: data) // Decodifica um array de CombinedCardModel
+            let decodedObject = try decoder.decode([CombinedCardModel].self, from: data)
             return decodedObject
         } catch {
             if let decodingError = error as? JSONReadingError {
---
+++ Ciganus/Ciganus/Main/Source.swift
@@ -0,0 +1,19 @@
+//
+//  Source.swift
+//  Ciganus
+//
+//  Created by Fernando Marins on 12/04/25.
+//
+
+import Foundation
+
+class Source {
+    static let shared = Source()
+    let nomesDasCartas: [String: String] = [
+        "1": "Cavaleiro", "2": "Trevo", "3": "Navio", "4": "Casa", "5": "Árvore", "6": "Nuvens", "7": "Cobra", "8": "Caixão",
+        "9": "Buquê", "10": "Foice", "11": "Chicote", "12": "Pássaros", "13": "Criança", "14": "Raposa", "15": "Urso", "16": "Estrelas",
+        "17": "Cegonha", "18": "Cachorro", "19": "Torre", "20": "Jardim", "21": "Montanha", "22": "Caminhos", "23": "Rato", "24": "Coração",
+        "25": "Anel", "26": "Livros", "27": "Carta", "28": "Homem", "29": "Mulher", "30": "Lírios", "31": "Sol", "32": "Lua",
+        "33": "Chave", "34": "Peixes", "35": "Âncora", "36": "Cruz"
+    ]
+}
